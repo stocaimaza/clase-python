f4804eb7dde29d4e08e7e8fc29dbeb99280b82e4
stocaimaza <stocaimaza@hotmail.com>
2025-12-09 20:57:06 -0300

    En la rama Gustavo generamos un print

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("Primer punto de guardado")
 
 cliente = "Samuel"
 apellidoCliente = "Tocaimaza"
+
+## Estamos trabajando en la rama Gustavo
+print("Estamos en la rama Gustavo creando el backend")
